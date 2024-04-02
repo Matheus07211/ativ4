b79e39053cc94792c5e08f7d9e52e26c8075baa7
Matheus <matheuslima0721@gmail.com>
2024-04-02 18:36:20 -0300

    Front

---
+++ publico/scripts/calculo.js
@@ -0,0 +1,20 @@
+function somarValores1(){
+    var s1 = document.getElementById("s1").value;
+    var t1 = parseInt(s1) * parseInt(15);
+    alert('O valor total é : R$'+ t1);
+}
+function somarValores2(){
+    var s2 = document.getElementById("s2").value;
+    var t2 = parseInt(s2) * parseInt(22);
+    alert('O valor total é : R$'+ t2);
+}
+function somarValores3(){
+    var s3 = document.getElementById("s3").value;
+    var t3 = parseInt(s3) * parseInt(35);
+    alert('O valor total é : R$'+ t3);
+}
+function somarValores4(){
+    var s4 = document.getElementById("s4").value;
+    var t4 = parseInt(s4) * parseInt(8);
+    alert('O valor total é : R$'+ t4);
+}
